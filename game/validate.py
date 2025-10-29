d461377b61802332b13f09bd1c4d9ef08f9fadcf
DavidPopovich3261 <8534397@gmail.com>
2025-10-29 14:47:19 +0200

    finish

--- game/validate.py
+++ game/validate.py
@@ -14,7 +14,7 @@ def is_valid_guess(guess: str, length: int, unique_digits: bool = True)->bool:
                     print()
                 else:
                     guess2+=i
-    return flag
+        return flag
 
 
 
