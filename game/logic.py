73f59776cd34524db07c68a1bca1503cc3191647
DavidPopovich3261 <8534397@gmail.com>
2025-10-29 11:48:55 +0200

    logic

--- game/logic.py
+++ game/logic.py
@@ -4,21 +4,31 @@
 
 
 def score_guess(secret: str, guess: str) -> tuple[int, int]:
-    pass
+    bulls=0
+    cows=0
+    for i,y in enumerate(guess):
+        if y in secret:
+            if guess[i]==secret[i]:
+                bulls+=1
+            else:
+                cows+=1
+    return bulls,cows
 
 
 def is_won(bulls: int, length: int) -> bool:
-    pass
+    if bulls==length:
+        return True
+    return False
 
 
 
 def init_state(secret: str, length: int, max_tries: int | None, unique_digits: bool, allow_leading_zero: bool) -> dict:
     state = {
-        "secret": str,
-        "length": int,
-        "max_tries": int | None,
-        "tries_used": int,
-        "unique_digits": bool,
+        "secret": secret,
+        "length": length,
+        "max_tries": max_tries,
+        "tries_used": 0,
+        "unique_digits": unique_digits,
         "allow_leading_zero": bool,
         "history": list[tuple[str, int, int]],  # [(guess, bulls, cows), ...]
         "seen": set[str]  # ניחושים שנעשו
@@ -26,5 +36,8 @@ def init_state(secret: str, length: int, max_tries: int | None, unique_digits: b
     return state
 
 
-def apply_guess(state: dict, guess: str) -> tuple[int, int]:
-    pass
+def apply_guess(state: dict, guess: str,bulls_cows:tuple[int, int]):
+    history=[guess,bulls_cows[0],bulls_cows[1]]
+    state['history'].append(history)
+    state['tries_used']+=1
+    state['seen'].append(guess)
