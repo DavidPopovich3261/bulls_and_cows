26ec950637bc17f4c1340bd3ff996920c8c6883e
DavidPopovich3261 <8534397@gmail.com>
2025-10-29 10:17:13 +0200

    validate

--- game/validate.py
+++ game/validate.py
@@ -1,12 +1,24 @@
 
 
-
-
-
 def is_valid_guess(guess: str, length: int, *, unique_digits: bool = True) -> tuple[bool, str]:
-    pass
+    flag=True
+    if len(guess)==length:
+        if not guess.isdigit():
+            flag=False
+            print()
+        elif unique_digits:
+            guess2=''
+            for i in guess:
+                if i in guess2:
+                    flag =False
+                    print()
+                else:
+                    guess2+=i
+    return flag ,guess
 
 
 
 def is_new_guess(guess: str, history: set[str]) -> bool:
-    pass
+    if guess in history:
+        return False
+    return True
